d6fa7db51e8e4060187fd898d19575dbaa41c1c9
ASHISHSINGHNEGI <ashishsinghnegi381@gmail.com>
2023-10-19 18:28:39 +0530

    3commit

--- readme.as
+++ readme.as
@@ -6,6 +6,7 @@ git commit -m " "
 
 
 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
+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
 
 1-cd "Open Elective Management System"
 2-npm install
--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  return (<>
-    <div>login branch checkout</div>
-    <div>ashishsingh negi</div>
+  return (
+    <>
+      <div>login branch checkout</div>
+
+      <div>ashishsingh negi</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
